1b9e4d26f401bb92cc7ba6a6a413f5771e65e4f6
Bill Sanderson <bill@towniebagels.com>
2025-07-10 08:50:00 -0700

    java sdk 11 usage clear red warnings in this app/build.gradel.kts update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,4 +1,5 @@
 //version before test java 11 SDK use update
+/*
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -29,10 +30,12 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
 
-       /* minSdk = flutter.minSdkVersion
+       */
+/* minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
-        versionName = flutter.versionName*/
+        versionName = flutter.versionName*//*
+
 
         minSdk = 23
         targetSdk = 34
@@ -65,5 +68,54 @@ dependencies {
 
     // https://firebase.google.com/docs/android/setup#available-libraries
 
+}
+
+*/
+//java 11 use below
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.section15"
+    compileSdk = 34
+    ndkVersion = "27.0.12077973"
+
+    defaultConfig {
+        applicationId = "com.example.section15"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
+    // If you need specific Firebase libraries, add them here, e.g.:
+    // implementation("com.google.firebase:firebase-analytics-ktx")
+    // implementation("com.google.firebase:firebase-auth-ktx")
 }
 
